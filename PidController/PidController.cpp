5e54f993f1c92a1e5dc250a807078409691d5737
jbenoit <jbenoit@t-online.de>
2018-10-19 00:15:28 +0200

    initial

--- PidController/PidController.cpp
+++ PidController/PidController.cpp
@@ -1,37 +1,22 @@
 #include "PidController.h"
 #include "Arduino.h"
 
-void PidController::initialize(int kp, int ki, int kd, int max_pwr, int max_d, int max_i, int intervall_millis)
+void PidController::initialize(int kp, int ki, int kd, int max_pwr, int max_i, int max_d, int intervall_millis)
 {
-    output_pin = o_pin;
-    signal_pin = s_pin;
-    hz_factor = 500000 * SAMPLES_AMOUNT;
-    netFreqCnt = 0;
-    max_power = max_pwr;
-
-    noInterrupts();
-    pinMode(signal_pin, INPUT);
-    attachInterrupt(digitalPinToInterrupt(signal_pin), ISR_acIsZero, RISING);
-
-    pinMode(output_pin, OUTPUT);
-
-    pcc_is_on = false;
-
-    // set up timer interrupt for Timer1
-    TCCR1A = 0;
-    TCCR1B = 0;
-
-    bitSet(TCCR1B, CS11);    // specify 8 als Prescaler for timer 1
-    bitSet(TIMSK1, OCIE1A);  // activate Timer output compare Interrupt A at timer 1
-
-    interrupts();             // activate all interrupts
-    OCR1A = 0;
-
-
+    this->kp = kp; 
+    this->ki = ki; 
+    this->kd = kd; 
+    this->max_pwr = max_pwr; 
+    this->max_i = max_i; 
+    this->max_d = max_d; 
+    this->intervall_millis = intervall_millis; 
+    
+ 
 }
 
 void PidController::regulate(int desired, int actual)
 {
+	
 	
     return;
 
--- PidController/PidController.h
+++ PidController/PidController.h
@@ -12,18 +12,20 @@ class PidController
 {
     public:
         // methods
-        void initialize(int kp, int ki, int kd, int max_pwr, int max_d, int max_i, int intervall_millis);
+        void initialize(int kp, int ki, int kd, int max_pwr, int max_i, int max_d, int intervall_millis);
         void regulate(int desired, int actual);
  
-        // parameters
-        unsigned int ocrDelta;
-        bool pcc_is_on;
-        byte signal_pin;
-        byte output_pin;
-        int max_power;
- 
-    protected:
     private:
+        // parameters
+        int kp; 
+        int ki; 
+        int kd;
+        int max_pwr;
+        int max_d;
+        int max_i; 
+        int intervall_millis; 
+        
+    protected:
 
 
 };
